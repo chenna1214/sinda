112a9e835a28c6fca6b65b6f76f128282c9892ad
chenna1214 <chennalily@qq.com>
2017-11-30 21:46:08 +0800

    陈娜+20171130+顶部底部1.0

--- sindaGrils/src/components/pcPublic/pcTop/pcTop.vue
+++ sindaGrils/src/components/pcPublic/pcTop/pcTop.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="pcTop">
    <!-- 最顶部 -->
-<p>欢迎来到信达！</p>
-<a href="#/userData/login">登录</a>
-<a href="#/userData/register">注册</a>
-<img src="" alt="">
-<p>购物车</p>
-<p>0</p>
-<p>件</p>
-<p>服务商入口</p>
+<p class="pcTopBlackText">欢迎来到信达！</p>
+<a href="#/userData/login"  class="pcTopBlueText">登录</a>
+<a href="#/userData/register"  class="pcTopBlueText">注册</a>
+<img src="../../images/pcTop/shoppingcarIcon.png" alt="购物车" class="pcShoppingcarIcon">
+<p class="pcTopBlackText">购物车</p>
+<p  class="pcTopBlueText">0</p>
+<p class="pcTopBlackText">件</p>
+<p  class="pcTopBlueText">服务商入口</p>
    </div>
 </template>
 
@@ -25,10 +25,23 @@ export default {
 </script>
 
 <style scoped lang='less'>
-
 .pcTop{
   display: flex;
+  align-items: center;
   background: #f2f2f2;
   height:35px;
+}
+.pcTopBlackText{
+  font-size: 12px;
+  
+}
+.pcTopBlueText{
+  color: #2794d5;
+  font-size: 12px;
+  text-decoration: none;
+}
+.pcShoppingcarIcon{
+  width:21px;
+  height:17px;
 }
 </style>
