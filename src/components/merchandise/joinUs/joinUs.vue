9e23a4aa763cc944afc2e3a520317109f9355993
chenna1214 <chennalily@qq.com>
2017-12-08 09:05:02 +0800

    陈娜+20171208+加盟我们/订单详情静态

--- src/components/merchandise/joinUs/joinUs.vue
+++ src/components/merchandise/joinUs/joinUs.vue
@@ -9,9 +9,9 @@
     <div class="pcjoinUsIn">
       <div class="pcSerPeo"><!-- 服务商 -->
         <img src="../../images/joinUs/serPeo.png" class="pcSerPeoImg">
-        <p class="pcGetCNTex">服务商加盟</p>
+        <p class="pcGetCNTex pcSerText">服务商加盟</p>
         <p class="pcWebAdr">www.xinda.com</p>
-        <p>信达提供线上精准营销方案、帮助服务商快捷、低成本的获取有效客户，为传统优质服务商插上互联网的翅膀，助力企业腾飞。</p>
+        <p class="pcPubTil">信达提供线上精准营销方案、帮助服务商快捷、低成本的获取有效客户，为传统优质服务商插上互联网的翅膀，助力企业腾飞。</p>
       </div>
     </div>
     <div class="pcGetBox"><!-- 你将获得 -->
@@ -21,18 +21,43 @@
       <p class="pcGetENTex">YOU WILL RECEIVE</p>
       <p class="pcGetCNTex">你将获得</p>
       <p class="pcWebAdr">www.xinda.com</p>
-      <el-row>
-        <el-col v-for='(getSin,index) in getList' :key="getSin">
-          <!-- <img src="../../images/joinUs/getIcon.png" :class="{goal:index==0}"> -->
-          <div :class="{goal:index==0}"></div>
+      <el-row class="pcGetDetail">
+        <el-col v-for='(getSin,index) in getList' :key="getSin" :sm="12" :md="12" :lg="12">
+          <div class="pcGetDetailInner">
+              <div :class="{pcGoal:index==0,pcIdea:index==1,pcDollar:index==2,pcCard:index==3}"></div>
+              <p class="pcGetDetailP">{{getSin}}</p>
+          </div> 
         </el-col>
       </el-row>
-
-<!-- <div class="goal"></div> -->
-
-
-
+      
     </div>
+
+
+
+    <div class="pcGetBox pcJob"><!-- 哪些职业可以加盟 -->
+      <div class="pcGetTilBox">
+        <img src="../../images/joinUs/job.png" class="pcGetTilImg">
+      </div>
+        <p class="pcGetENTex">PROFESSIONAL</p>
+        <p class="pcGetCNTex">哪些职业可以加盟</p>
+        <p class="pcWebAdr">www.xinda.com</p>
+      <div class="pcjoinUsIn">
+      <el-row>
+        <el-col :sm="6" :md="6" :lg="6" v-for="(pcJob,index) in pcJobs" :key="pcJob.jobTil" class="pcJobInner">
+          <div @mouseover="pcJobOver(index)">
+            <div class="pcJobIconBox">
+              <div class="pcJobIcon" :class="{pcNote:index==0,pcAnchor:index==1,pcBook:index==2,pcPeople:index==3}"></div>
+            </div>
+            <p class="pcJobTil" :class="{pcJobOver:pcJobO==index}">{{pcJob.jobTil}}</p>
+            <p class="pcJobInfo" :class="{pcJobOver:pcJobO==index}">{{pcJob.jobInfo}}</p>
+          </div>
+        </el-col>
+      </el-row>
+      <p class="pcPubTil">我们需要什么样的服务商？</p>
+      <div class="pcJobDetailBox"><p class="pcJobDetail">{{serviceDetail}}</p></div>
+    </div>
+    </div>
+
    </div>
 </template>
 
@@ -41,10 +66,48 @@ export default {
   name: "joinUs",
   data() {
     return {
-      getList:[//你将获得
-        '精准的线上曝光机会','万亿市场份额','系统化的营销解决方案','各种补贴优惠'
+      index:'',
+      pcJobO:-1,
+      getList: [
+        //你将获得
+        "精准的线上曝光机会",
+        "万亿市场份额",
+        "系统化的营销解决方案",
+        "各种补贴优惠"
+      ],
+      serviceDetail: "在公司注册、公司变更、资质审批等领域深耕多年，在业内享有一定的知名度，可以提供优质优价的服务；与时俱进，视野立足未来，了解创业者的需求，并乐于为创业者服务。",
+      pcJobDetailList: [
+        "在公司注册、公司变更、资质审批等领域深耕多年，在业内享有一定的知名度，可以提供优质优价的服务；与时俱进，视野立足未来，了解创业者的需求，并乐于为创业者服务。",
+        "在代理记账、审计验资等领域深耕多年，可以提供涉足各个行业优秀的财会人员；与时俱进，视野立足未来，了解创业者的需求，并乐于为创业者服务。",
+        "在商标注册，版权专利，著作权等领域深耕多年，具有丰富的知识产权经验；与时俱进，视野立足未来，了解创业者的需求，并乐于为创业者服务。",
+        "在人事代理、企业社保、个人社保等领域深耕多年，具有丰富的社保代理经验。"
+      ],
+      pcJobs: [
+        //哪些职业可以加盟
+        {
+          jobTil: "工商注册",
+          jobInfo: "REGISTERED"
+        },
+        {
+          jobTil: "财税服务",
+          jobInfo: "SERVICE"
+        },
+        {
+          jobTil: "知识产权",
+          jobInfo: "INTELLECTUAL PROPERTY"
+        },
+        {
+          jobTil: "人力外包",
+          jobInfo: "HR OUTSOURCING"
+        }
       ]
     };
+  },
+  methods:{
+    pcJobOver:function(index){//哪些职业可以加盟
+      this.serviceDetail=this.pcJobDetailList[index];
+      this.pcJobO=index;
+    }
   }
 };
 </script>
@@ -53,16 +116,29 @@ export default {
 //公共样式
 .pcGetENTex {
   font-size: 20px;
+  text-align: center;
+  margin-top: -8%;
 }
 .pcGetCNTex {
   font-size: 28px;
   color: red;
+  text-align: center;
+  margin-top: 3.5%;
+}
+.pcSerText {
   margin-top: -39.5%;
 }
 .pcWebAdr {
   font-size: 8px;
   color: #585858;
   margin-top: 0.5%;
+  text-align: center;
+}
+.pcPubTil {
+  font-size: 20px;
+  margin-top: 4%;
+  font-weight: bold;
+  text-align: center;
 }
 //头部
 .pcjoinUsIn {
@@ -95,30 +171,10 @@ export default {
     margin-left: 24.3%;
     width: 56%;
   }
-  p {
-    text-align: center;
-  }
-  p:nth-child(4) {
-    font-size: 20px;
-    margin-top: 4%;
-    font-weight: bold;
-  }
 }
 //你将获得
 .pcGetBox {
-  border-top: 1px solid #000;
-  border-bottom: 1px solid #000;
   background: #edf0f3;
-  height: 566px;
-  p {
-    text-align: center;
-  }
-  p:nth-child(2) {
-    margin-top: -8%;
-  }
-  p:nth-child(3){
-    margin-top: 3.5%;
-  }
 }
 .pcGetTilBox {
   display: flex;
@@ -127,10 +183,96 @@ export default {
     margin-top: 5%;
   }
 }
-.goal{
+.pcGoal {
   background: url(../../images/joinUs/getIcon.png) 0 0 no-repeat;
-  height: 79px;
+  height: 75px;
+  width: 75px;
+}
+.pcIdea {
+  background: url(../../images/joinUs/getIcon.png) -176px 0 no-repeat;
+  height: 80px;
+  width: 77px;
+}
+.pcDollar {
+  background: url(../../images/joinUs/getIcon.png) 0 -163px no-repeat;
+  height: 61px;
+  width: 82px;
+}
+.pcCard {
+  background: url(../../images/joinUs/getIcon.png) -176px -163px no-repeat;
+  height: 57px;
   width: 79px;
 }
+.pcGetDetail {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 3%;
+  margin-left: 24%;
+}
+.pcGetDetailP {
+  font-size: 17px;
+  margin-left: 85px;
+  margin-top: -45px;
+}
+.pcGetDetailInner {
+  margin-bottom: 19.33%;
+}
 
+.pcJob {
+  background: #fff;
+}
+.pcJobTil {
+  font-size: 17px;
+  font-weight: bold;
+  padding-top: 37px;
+}
+.pcJobInfo {
+  font-size: 11px;
+  color: #878787;
+}
+.pcJobIcon {
+  background: url(../../images/joinUs/jobIcon.png) no-repeat;
+  max-width: 138px;
+  width: 70%;
+  padding-bottom: 153px;
+}
+.pcNote {
+  background-position: 0 0;
+}
+.pcAnchor {
+  background-position: -242px 0;
+}
+.pcBook {
+  background-position: 0 -290px;
+}
+.pcPeople {
+  background-position: -242px -290px;
+}
+.pcJobInner {
+  text-align: center;
+  margin-top: 47px;
+}
+.pcJobIconBox {
+  display: flex;
+  justify-content: center;
+}
+.pcJobDetail{
+  font-size: 17px;
+  text-align: center;
+  margin-top: 27px;
+  width:60%;
+  color:#a3a3a3;
+  padding-bottom: 96px;
+}
+.pcJobDetailBox{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.pcJobOver{
+  color: #2693d4;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import shop from '../components/merchandise/shop/shop.vue'
 import failurePay from '../components/merchandise/order/failurePay.vue'//支付失败
 import paySuccess from '../components/merchandise/order/paySuccess.vue'//支付成功
 import pc_shophp from '../components/merchandise/shop/pc_shophp.vue'
+import goodsOrder from '../components/merchandise/order/goodsOrder.vue'//订单详情
 //登录、注册、忘记密码网页
 import userData from '../components/userData/userData.vue'
 import login from '../components/userData/login.vue'
@@ -29,100 +30,98 @@ import userevalua from '../components/merchandise/memberCenter/userevalua.vue'
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-      {
-        path: '/userData',//登录、注册、忘记密码网页
-        name: 'userData',
-        component: userData,
-        children:[
+  routes: [
+    {
+      path: '/userData',//登录、注册、忘记密码网页
+      name: 'userData',
+      component: userData,
+      children: [
+        {
+          path: 'login',//登录
+          name: 'login',
+          component: login
+        },
+        {
+          path: 'register',//注册
+          name: 'register',
+          component: register
+        },
+        {
+          path: 'forgetPassword',//忘记密码
+          name: 'forgetPassword',
+          component: forgetPassword
+        },
+      ]
+    },
+    {
+      path: '/merchandise',//非登录、注册、忘记密码网页
+      name: 'merchandise',
+      component: merchandise,
+      children: [
+        {
+          path: 'allProduct',//全部产品
+          name: 'allProduct',
+          component: allProduct
+        },
+        {
+          path: 'taxationService',//财税服务
+          name: 'taxationService',
+          component: taxationService
+        },
+        {
+          path: 'productdetail',//商品详情
+          name: 'productdetail',
+          component: productdetail
+        },
+        {
+          path: 'companyIndustry',//公司工商
+          name: 'companyIndustry',
+          component: companyIndustry
+        },
+        {
+          path: 'joinUs',//加盟我们
+          name: 'joinUs',
+          component: joinUs
+        },
+        {
+          path: 'shop',//店铺
+          name: 'shop',
+          component: shop
+        },
+        {
+          path: 'pc_shophp',//店铺首页
+          name: 'pc_shophp',
+          component: pc_shophp
+        },
+        {
+          path: 'membercenter',//会员中心网页，公共
+          name: 'membercenter',
+          component: membercenter,
+          children: [
             {
-                path: 'login',//登录
-                name: 'login',
-                component: login
-              },
-              {
-                path: 'register',//注册
-                name: 'register',
-                component: register
-              },
-              {
-                path: 'forgetPassword',//忘记密码
-                name: 'forgetPassword',
-                component: forgetPassword
-              },
-        ]
-      },
-      {
-        path: '/merchandise',//非登录、注册、忘记密码网页
-        name: 'merchandise',
-        component: merchandise,
-        children:[
+              path: 'member',//会员中心
+              name: 'member',
+              component: member,
+
+            },
             {
-                path: 'allProduct',//全部产品
-                name: 'allProduct',
-                component: allProduct
-              },
-              {
-                path: 'taxationService',//财税服务
-                name: 'taxationService',
-                component: taxationService
-              },
-              {
-                path: 'productdetail',//商品详情
-                name: 'productdetail',
-                component: productdetail
-              },
-              {
-                path: 'companyIndustry',//公司工商
-                name: 'companyIndustry',
-                component: companyIndustry
-              },
-              {
-                path: 'joinUs',//加盟我们
-                name: 'joinUs',
-                component: joinUs
-              }, 
-              {
-                path: 'shop',//店铺
-                name: 'shop',
-                component: shop
-              },
-              {
-                path: 'pc_shophp',//店铺首页
-                name: 'pc_shophp',
-                component: pc_shophp
-              },
-              {
-                path: 'membercenter',//会员中心网页，公共
-                name: 'membercenter',
-                component: membercenter,
-                children:[
-                  {
-                    path: 'member',//会员中心
-                    name: 'member',
-                    component: member,
-                    // children:[
-                    //   {
-                    //     path:'',//订单
-                    //     name:'',
-                    //     component:
-                    //   }
-                    // ]
-                  },
-                  {
-                    path: 'userevalua',//用户评价
-                    name: 'userevalua',
-                    component: userevalua
-                  },
-                  {
-                    path: 'memaccount',//账户设置
-                    name: 'memaccount',
-                    component: memaccount
-                  },
-                ]
-              },
-        ]
-      },
-    ]
-  })
-  
+              path: 'userevalua',//用户评价
+              name: 'userevalua',
+              component: userevalua
+            },
+            {
+              path: 'memaccount',//账户设置
+              name: 'memaccount',
+              component: memaccount
+            },
+          ]
+        },
+        {
+          path: 'goodsOrder',//订单
+          name: 'goodsOrder',
+          component: goodsOrder
+        }
+      ]
+    },
+  ]
+})
