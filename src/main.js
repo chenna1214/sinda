aa9767374e7bb0652afb09bb5c8ea3cae4cbd585
liuwenyue <1790513208@qq.com>
2017-12-11 11:13:17 +0800

    20171211刘雯月上传

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css';
-
+import qs from 'qs'
 
 import axios from 'axios'
 import Distpicker from 'v-distpicker'
@@ -12,6 +12,7 @@ import Distpicker from 'v-distpicker'
 // 三级联动
 Vue.component('v-distpicker', Distpicker)
 Vue.prototype.ajax = axios;
+Vue.prototype.qs = qs;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
