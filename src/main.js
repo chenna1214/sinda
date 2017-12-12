b6b0d98e67e3dd0ef2c4a90252154d92de4ae891
chenna1214 <chennalily@qq.com>
2017-12-12 08:35:48 +0800

    陈娜+20171212+模糊搜索/购物车计件

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css';
-
-
 import axios from 'axios'
+import qs from 'qs'
 import Distpicker from 'v-distpicker'
+import store from './store'
 
 // 三级联动
 Vue.component('v-distpicker', Distpicker)
 Vue.prototype.ajax = axios;
+Vue.prototype.qs = qs;
+Vue.prototype.qs = qs;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
@@ -20,6 +22,7 @@ Vue.use(ElementUI);
 new Vue({
   el: '#sinda',
   router,
+  store,
   template: '<sinda/>',
   components: { sinda }
 })
